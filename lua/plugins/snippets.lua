ff6a827d98442464c7f19528cde41830c8db7ef8
Naowal Rahman <naowal.ar@gmail.com>
2025-02-28 11:54:22 -0500

    removed redundant configuration from snippets.lua

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,32 +1,8 @@
 return {
     {
-        "L3MON4D3/LuaSnip",
-        lazy = true,
-        build = (not LazyVim.is_win())
-                and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
-            or nil,
-        dependencies = {
-            {
-                "rafamadriz/friendly-snippets",
-                config = function()
-                    require("luasnip.loaders.from_vscode").lazy_load {
-                        paths = { vim.fn.stdpath "config" .. "/snippets" },
-                    }
-                end,
-            },
-        },
-        opts = {
-            history = true,
-            delete_check_events = "TextChanged",
-        },
-    },
-    {
         "iurimateus/luasnip-latex-snippets.nvim",
         lazy = true,
         requires = { "L3MON4D3/LuaSnip", "lervag/vimtex" },
-        config = function()
-            require("luasnip-latex-snippets").setup()
-            -- require("luasnip").config.setup { enable_autosnippets = true }
-        end,
+        config = function() require("luasnip-latex-snippets").setup() end,
     },
 }
