e272ca61b2f44887a01360d83705150c2ae6bf57
Naowal Rahman <naowal.ar@gmail.com>
2025-08-04 13:14:22 -0400

    nightfox theme

---
+++ lua/plugins/nightfox.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        "EdenEast/nightfox.nvim",
+        opts = {
+            options = {
+                styles = {
+                    comments = "NONE",
+                    conditionals = "bold",
+                    keywords = "bold",
+                    functions = "NONE",
+                    strings = "NONE",
+                    numbers = "NONE",
+                    types = "NONE",
+                    operators = "NONE",
+                    variables = "NONE",
+                },
+            },
+        },
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "carbonfox",
+        },
+    },
+}
