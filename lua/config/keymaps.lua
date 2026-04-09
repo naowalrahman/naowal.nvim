1d8c2e0fc0fdf9d34c9dba694d0d27dca63bd3a4
Naowal Azraf Rahman <naowal.ar@gmail.com>
2026-04-09 13:59:33 -0400

    neovim and tmux cross navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,3 +27,12 @@ vim.keymap.set(
 
 -- open code actions
 vim.keymap.set("n", "<C-x>", function() vim.lsp.buf.code_action() end, { noremap = true, silent = true })
+
+-- neovim + tmux cross navigation
+local nvim_tmux_nav = require "nvim-tmux-navigation"
+vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
---
+++ lua/plugins/tmux.lua
@@ -0,0 +1,4 @@
+return {
+    "alexghergh/nvim-tmux-navigation",
+    config = function() require("nvim-tmux-navigation").setup { disable_when_zoomed = true } end,
+}
